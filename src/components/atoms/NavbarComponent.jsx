69c6e51cc67f7684b70acf790f9399744a7be7cf
gmlwisemail2022 <gmlwisemail2004@yahoo.com>
2024-07-06 14:29:43 +0800

    Update NavbarComponent.jsx

--- src/components/atoms/NavbarComponent.jsx
+++ src/components/atoms/NavbarComponent.jsx
@@ -15,6 +15,7 @@ const NavbarComponent = () => {
   }, []);
 
   const handleHeroClick = () => {
+    console.log("navigte via click");
     const heroSection = document.getElementById("hero-section");
     if (heroSection) {
       heroSection.scrollIntoView({ behavior: "smooth" });
@@ -22,6 +23,7 @@ const NavbarComponent = () => {
   };
 
   const handleAboutClick = () => {
+    console.log("navigte via click");
     const aboutSection = document.getElementById("about-section");
     if (aboutSection) {
       aboutSection.scrollIntoView({ behavior: "smooth" });
@@ -29,6 +31,7 @@ const NavbarComponent = () => {
   };
 
   const handleExperienceClick = () => {
+    console.log("navigte via click");
     const experienceSection = document.getElementById("experience-section");
     if (experienceSection) {
       experienceSection.scrollIntoView({ behavior: "smooth" });
@@ -36,6 +39,7 @@ const NavbarComponent = () => {
   };
 
   const handleSkillsClick = () => {
+    console.log("navigte via click");
     const skillsSection = document.getElementById("skills-section");
     if (skillsSection) {
       skillsSection.scrollIntoView({ behavior: "smooth" });
@@ -43,6 +47,7 @@ const NavbarComponent = () => {
   };
 
   const handlePortfolioClick = () => {
+    console.log("navigte via click");
     const portfolioSection = document.getElementById("portfolio-section");
     if (portfolioSection) {
       portfolioSection.scrollIntoView({ behavior: "smooth" });
